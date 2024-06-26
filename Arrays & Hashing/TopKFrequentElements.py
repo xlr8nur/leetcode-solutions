64502142f8a2a9bcb0e4edab165d62c3fce62b94
Nur <justnur13@gmail.com>
2024-06-26 18:34:44 +0300

    TopKFrequentElements.py

--- Arrays & Hashing/TopKFrequentElements.py
+++ Arrays & Hashing/TopKFrequentElements.py
@@ -1,3 +1,11 @@
+'''
+Question;
+
+Given an integer array nums and an integer k, return the k most frequent elements within the array.
+The test cases are generated such that the answer is always unique.
+You may return the output in any order.
+'''
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
