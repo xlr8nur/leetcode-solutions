0609804005b8ad19e88a556eb783033cd8799725
Nur <justnur13@gmail.com>
2024-06-26 18:33:49 +0300

    ProductofArrayExceptSelf.py

--- Arrays & Hashing/ProductofArrayExceptSelf.py
+++ Arrays & Hashing/ProductofArrayExceptSelf.py
@@ -1,3 +1,11 @@
+'''
+Question;
+
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+Each product is guaranteed to fit in a 32-bit integer.
+Follow-up: Could you solve it in O(n)O(n) time without using the division operation?
+'''
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
