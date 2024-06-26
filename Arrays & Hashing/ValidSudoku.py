b4e93398ff709f26ee63c34f2e348121c0af663e
Nur <justnur13@gmail.com>
2024-06-26 18:37:19 +0300

    ValidSudoku.py

--- Arrays & Hashing/ValidSudoku.py
+++ Arrays & Hashing/ValidSudoku.py
@@ -1,3 +1,16 @@
+'''
+Question;
+
+You are given a a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+    Each row must contain the digits 1-9 without duplicates.
+    Each column must contain the digits 1-9 without duplicates.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+
+Return true if the Sudoku board is valid, otherwise return false
+Note: A board does not need to be full or be solvable to be valid.
+'''
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         cols = collections.defaultdict(set)
