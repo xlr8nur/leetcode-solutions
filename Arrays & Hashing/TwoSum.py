98c2f49f85cc3c0e4cbd2bc297533974f9307590
Nur <justnur13@gmail.com>
2024-06-26 18:35:40 +0300

    Update TwoSum.py

--- Arrays & Hashing/TwoSum.py
+++ Arrays & Hashing/TwoSum.py
@@ -1,3 +1,11 @@
+'''
+Question;
+
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+Return the answer with the smaller index first. 
+'''
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}  # val -> index
