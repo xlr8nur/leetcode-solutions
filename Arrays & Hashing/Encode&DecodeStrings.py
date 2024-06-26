bd41894821cfe44631579cfb77b566acd95bb101
Nur <justnur13@gmail.com>
2024-06-26 18:28:17 +0300

    Encode&DecodeStrings.py

--- Arrays & Hashing/Encode&DecodeStrings.py
+++ Arrays & Hashing/Encode&DecodeStrings.py
@@ -1,3 +1,10 @@
+'''
+Question;
+
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+'''
+
 class Solution:
     def encode(self, strs):
         res = ""
