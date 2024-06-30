9d320486b4416b4e6e3d2ee7269450f5dd767477
Nur <justnur13@gmail.com>
2024-06-30 10:33:13 +0300

    BinarySearch.py

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -4,6 +4,7 @@ Question;
 You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
 Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
 Your solution must run in O(logn)O(logn) time.
+
 '''
 
 class Solution:
