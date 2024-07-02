a160002ce342b8e210d04ea8315969e809a30d90
Nur <justnur13@gmail.com>
2024-07-02 18:30:26 +0300

    RemoveNthNode.py

--- Linked List/RemoveNthNode.py
+++ Linked List/RemoveNthNode.py
@@ -3,6 +3,7 @@ Question;
 
 You are given the beginning of a linked list head, and an integer n.
 Remove the nth node from the end of the list and return the beginning of the list.
+
 '''
 
 class Solution:
