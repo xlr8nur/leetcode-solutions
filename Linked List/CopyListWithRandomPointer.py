ccbe5665722a8a7b09f86213e9eb90245f707fee
Nur <justnur13@gmail.com>
2024-07-02 18:26:53 +0300

    CopyListWithRandomPointer.py

--- Linked List/CopyListWithRandomPointer.py
+++ Linked List/CopyListWithRandomPointer.py
@@ -2,9 +2,7 @@
 Question;
 
 You are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
-
 Create a deep copy of the list.
-
 The deep copy should consist of exactly n new nodes, each including:
 
     The original value val of the copied node
@@ -12,19 +10,8 @@ The deep copy should consist of exactly n new nodes, each including:
     A random pointer to the new node corresponding to the random pointer of the original node
 
 Note: None of the pointers in the new list should point to nodes in the original list.
-
 Return the head of the copied linked list.
 '''
-
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
-"""
-
 
 class Solution:
     def copyRandomList(self, head: "Node") -> "Node":
