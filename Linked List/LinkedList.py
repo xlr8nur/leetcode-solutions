7e80fb4d78bb64648521be0830e8e0892bfd1219
Nur <justnur13@gmail.com>
2024-07-02 18:28:15 +0300

    LinkedList.py

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -3,13 +3,6 @@ Question;
 Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
 '''
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev, curr = None, head
