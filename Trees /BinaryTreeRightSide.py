05955b44b8ca9564689f7a3afb71afe972b96287
Nur <justnur13@gmail.com>
2024-07-03 16:46:32 +0300

    BinaryTreeRightSide.py

---
+++ Trees /BinaryTreeRightSide.py
@@ -0,0 +1,24 @@
+'''
+Question;
+
+You are given the root of a binary tree. Return only the values of the nodes that are visible from the right side of the tree, ordered from top to bottom.
+'''
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
