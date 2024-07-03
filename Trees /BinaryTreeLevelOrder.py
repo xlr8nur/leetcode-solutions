1e4a99bcdaef201f401f4a833f5638fef57d0bf1
Nur <justnur13@gmail.com>
2024-07-03 16:44:36 +0300

    BinaryTreeLevelOrder.py

---
+++ Trees /BinaryTreeLevelOrder.py
@@ -0,0 +1,25 @@
+'''
+Question;
+
+Given a binary tree root, return the level order traversal of it as a nested list, where each sublist contains the values of nodes at a particular level in the tree, from left to right.
+'''
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        if root:
+            q.append(root)
+
+        while q:
+            val = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val)
+        return res
