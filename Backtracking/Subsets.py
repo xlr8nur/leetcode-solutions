5fc5e206cbc0244cd1bf1e95eb555aa94fa91a95
Nur <justnur13@gmail.com>
2024-07-05 20:57:19 +0300

    Subsets.py

---
+++ Backtracking/Subsets.py
@@ -0,0 +1,26 @@
+'''
+Question;
+
+Given an array nums of unique integers, return all possible subsets of nums.
+The solution set must not contain duplicate subsets. You may return the solution in any order.
+'''
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
