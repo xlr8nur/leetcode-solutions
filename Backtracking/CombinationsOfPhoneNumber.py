e421344b950206fae8d95a2a8c1f8837801a5963
Nur <justnur13@gmail.com>
2024-07-05 21:07:50 +0300

    CombinationsOfPhoneNumber.py

---
+++ Backtracking/CombinationsOfPhoneNumber.py
@@ -0,0 +1,33 @@
+'''
+Question;
+
+You are given a string digits made up of digits from 2 through 9 inclusive.
+A digit could represent any one of the characters it maps to.
+Return all possible letter combinations that digits could represent. You may return the answer in any order.
+'''
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
