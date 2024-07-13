2a05905b732d6dcff7736ef01a4f3a08bb8def79
Nur <justnur13@gmail.com>
2024-07-13 21:37:38 +0300

    CloneGraph.py

---
+++ Graphs/CloneGraph.py
@@ -0,0 +1,31 @@
+'''
+Question;
+
+Given a node in a connected undirected graph, return a deep copy of the graph.
+Each node in the graph contains an integer value and a list of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+
+The graph is shown in the test cases as an adjacency list. An adjacency list is a mapping of nodes to lists, used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+For simplicity, nodes values are numbered from 1 to n, where n is the total number of nodes in the graph. The index of each node within the adjacency list is the same as the node's value (1-indexed).
+The input node will always be the first node in the graph and have 1 as the value.
+'''
+
+class Solution:
+    def cloneGraph(self, node: "Node") -> "Node":
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+
+        return dfs(node) if node else None
