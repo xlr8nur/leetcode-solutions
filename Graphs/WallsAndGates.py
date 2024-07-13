3ca431a58d74f7037617de8937113c04a217ae28
Nur <justnur13@gmail.com>
2024-07-13 21:39:24 +0300

    WallsAndGates.py

---
+++ Graphs/WallsAndGates.py
@@ -0,0 +1,52 @@
+'''
+Question;
+
+You are given a m×nm×n 2D grid initialized with these three possible values:
+
+    -1 - A water cell that can not be traversed.
+    0 - A treasure chest.
+    INF - A land cell that can be traversed. We use the integer 2^31 - 1 = 2147483647 to represent INF.
+
+Fill each land cell with the distance to its nearest treasure chest. If a land cell cannot reach a treasure chest than the value should remain INF.
+Assume the grid can only be traversed up, down, left, or right.
+'''
+class Solution:
+    """
+    @param rooms: m x n 2D grid
+    @return: nothing
+    """
+
+    def walls_and_gates(self, rooms: List[List[int]]):
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visit = set()
+        q = deque()
+
+        def addRooms(r, c):
+            if (
+                min(r, c) < 0
+                or r == ROWS
+                or c == COLS
+                or (r, c) in visit
+                or rooms[r][c] == -1
+            ):
+                return
+            visit.add((r, c))
+            q.append([r, c])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                rooms[r][c] = dist
+                addRooms(r + 1, c)
+                addRooms(r - 1, c)
+                addRooms(r, c + 1)
+                addRooms(r, c - 1)
+            dist += 1
+
